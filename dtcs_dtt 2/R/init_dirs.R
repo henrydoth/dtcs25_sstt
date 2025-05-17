b1a795e081028d18c601708040f0a1cd96eeffe1
drhenrydo <90409052+drhenrydo@users.noreply.github.com>
2025-05-17 11:01:47 +0700

    xóa dtt_2 và add 2file tham kao sstt mach mau va lwey body

--- dtcs_dtt 2/R/init_dirs.R
+++
@@ -1,9 +0,0 @@
-library(here)
-library(fs)
-
-fs::dir_create(here("output"))
-fs::dir_create(here("source"))
-fs::dir_create(here("image", "pdf"))
-fs::dir_create(here("others"))
-
-message("✅ Đã tạo hoặc xác nhận các thư mục: output/, source/, image/pdf/, others/")
--- dtcs_dtt 2/R/packages.R
+++
@@ -1,9 +0,0 @@
-if (!require("pacman")) install.packages("pacman")
-
-pacman::p_load(
-  dplyr, tidyr, forcats, haven,
-  ggplot2, lubridate, glue, flextable,
-  officer, officedown, jpeg, png, grid,
-  tidyverse, purrr, RColorBrewer, janitor,
-  effectsize, patchwork, scales, stringr
-)
